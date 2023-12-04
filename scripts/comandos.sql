393b11f1d2356e45729c7d313ff9e0ba2a94516b
Camily de bem <camily.bem@compjunior.com.br>
2023-12-04 13:28:07 -0300

    scripts a fazer

---
+++ scripts/comandos.sql
@@ -0,0 +1,35 @@
+-- (a) Criação de todas as tabelas e de todas as restrições de integridade.
+
+-- (b) Exemplos de ALTER TABLE e DROP TABLE.
+
+-- (c) Exemplo de uma inserção de dados em cada uma das tabelas.
+
+-- (d) Exemplos de modificação de dados em 5 tabelas.
+
+-- (e) Exemplos de exclusão de dados em 5 tabelas.
+
+-- (f) Exemplos de, pelo menos, 12 consultas. Inclua consultas simples e complexas,
+-- envolvendo todas as cláusulas do comando de consulta (SELECT, FROM, WHERE,
+-- ORDER BY, GROUP BY, HAVING), os operadores (JOIN, OUTER JOIN,
+-- UNION, AND, OR, NOT, BETWEEN, IN, LIKE, IS NULL, ANY/SOME, ALL,
+-- EXISTS), além de funções agregadas e consultas aninhadas (subconsultas).
+-- Não faça aninhamentos "forçados", somente os use em situações onde é difícil
+-- escrever uma consulta sem aninhamento. Será avaliado o nível de complexidade
+-- das consultas apresentadas. Não se esqueça de descrever em detalhes o que
+-- cada consulta recupera (ex: recupera o nome e o endereço dos gerentes dos
+-- departamentos que controlam os projetos localizados em Lavras) (valor: 50%);
+
+-- (g) Exemplos de criação de de 3 visões (Views). Inclua também exemplos de como usar
+-- cada uma das visões (valor: 5%);
+
+-- (h) Exemplos de criação de usuários (pelo menos 2), concessão (GRANT) e revocação
+-- (REVOKE) de permissão de acesso (valor: 5%);
+
+-- (i) Exemplos de 3 procedimentos/funções, com e sem parâmetros, de entrada e de saída,
+-- contendo alguns comandos tais como IF, CASE WHEN, WHILE, declaração de
+-- variáveis e funções prontas. Os procedimentos devem ter aplicação no seu projeto.
+-- Inclua exemplos de como executar esses procedimentos/funções (valor: 10%);
+
+-- (j) Exemplos de 3 triggers, um para cada evento (inserção, alteração e exclusão).
+-- Os triggers devem ter aplicação no seu projeto. Inclua exemplos de como disparar
+-- os triggers (valor: 10%).
