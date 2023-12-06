1d4c5a6d8724fa6c636388615f564e393043d390
Camily de bem <camily.bem@compjunior.com.br>
2023-12-06 18:03:17 -0300

    inserindo dados

--- scripts/letraF.sql
+++ scripts/letraF.sql
@@ -1,14 +1,3 @@
-/* exemplos de, pelo menos, 12 consultas. Inclua consultas simples e complexas,
-envolvendo todas as cláusulas do comando de consulta (SELECT, FROM, WHERE,
-ORDER BY, GROUP BY, HAVING), os operadores (JOIN, OUTER JOIN,
-UNION, AND, OR, NOT, BETWEEN, IN, LIKE, IS NULL, ANY/SOME, ALL,
-EXISTS), além de funções agregadas e consultas aninhadas (subconsultas). Não faça
-aninhamentos "forçados", somente os use em situações onde é difícil escrever uma
-consulta sem aninhamento. Será avaliado o nível de complexidade das consultas
-apresentadas. Não se esqueça de descrever em detalhes o que cada consulta
-recupera (ex: recupera o nome e o endereço dos gerentes dos departamentos que
-controlam os projetos localizados em Lavras) (valor: 50%); */	
-
 /* Recupera o nome, cidade e estado de todas as pessoas no banco de dados. */ 
 SELECT Nome, Cidade, Estado
 FROM restaurante.Pessoa;
@@ -86,7 +75,6 @@ WHERE EXISTS (
     HAVING COUNT(*) >= 2
 );
 
-
 /* Recupera o nome e a especialidade dos cozinheiros, incluindo aqueles sem especialidade associada. */
 SELECT Pessoa.Nome, Cozinheiro.Especialidade
 FROM restaurante.Pessoa
@@ -102,16 +90,16 @@ WHERE Cozinheiro.CPF = ANY (
     FROM restaurante.ItemDoMenu
 );
 
-
-/* Recupera o nome dos cozinheiros cujo salário é maior que todos os garçons. */
+-- Recupera o nome do funcionário cujo salário é maior que todos os outros funcionários
 SELECT Pessoa.Nome
 FROM restaurante.Pessoa
-JOIN restaurante.Cozinheiro ON Pessoa.CPF = Cozinheiro.CPF
-WHERE .Salario > ALL (
+JOIN restaurante.Funcionario ON Pessoa.CPF = Funcionario.CPF
+WHERE Funcionario.Salario > ALL (
     SELECT Salario
     FROM restaurante.Funcionario
-    WHERE CPF IN (SELECT CPF FROM restaurante.Garcom)
+    WHERE Funcionario.CPF <> Pessoa.CPF -- Exclui o próprio funcionário da comparação
 );
+
 
 /* Calcula a média do valor dos pedidos para cada garçom. */
 SELECT Garcom.CPF, AVG(Pedido.Valor) AS MediaPedidos
@@ -127,14 +115,13 @@ JOIN restaurante.Garcom ON Pedido.CPF_Garcom = Garcom.CPF
 ORDER BY Pedido.DataHora DESC
 LIMIT 5;
 
-/* retorna todos os pedidos cujo valor é maior que todos os pedidos feitos pelo cliente com CPF '12345678901' */
+/* retorna todos os pedidos cujo valor é maior que todos os pedidos feitos pelo cliente com CPF '18181818181' */
 SELECT *
 FROM restaurante.Pedido
-
 WHERE Valor > ALL (
   SELECT Valor
   FROM restaurante.Pedido
-  WHERE CPF_Cliente = '17171717171'
+  WHERE CPF_Cliente = '18181818181'
 );
 
 
--- scripts/letraG.sql
+++ scripts/letraG.sql
@@ -1,21 +1,23 @@
--- Seleciona o banco de dados onde você deseja criar as visões
 USE restaurante;
--- Criação da visão CozinheirosAtivos
+
+/* Criação da visão CozinheirosAtivos */
 CREATE VIEW CozinheirosAtivos AS
 SELECT Pessoa.CPF, Pessoa.Nome, Cozinheiro.Especialidade
 FROM restaurante.Pessoa
 JOIN restaurante.Cozinheiro ON Pessoa.CPF = Cozinheiro.CPF;
 
+/* exemplo de uso - verificar quais cozinheiros são mais ativos no restaurante*/
 SELECT * FROM CozinheirosAtivos;
 
--- Criação da visão que soma os salários de todos os funcionários
+/*  Criação da visão que soma os salários de todos os funcionários */
 CREATE VIEW SomaSalariosFuncionarios AS
 SELECT SUM(Salario) AS SomaSalarios
 FROM restaurante.Funcionario;
 
+/* exemplo de uso - cálculo de gastos mensais com funcionários */
 SELECT * FROM SomaSalariosFuncionarios;
 
--- Criação da visão que conta o número de pedidos para cada item do menu
+/* Criação da visão que conta o número de pedidos para cada item do menu */
 CREATE VIEW PedidosPorItemDoMenu AS
 SELECT
     ItemDoMenu.idItem,
@@ -27,5 +29,6 @@ LEFT JOIN
     restaurante.Pedido ON ItemDoMenu.idItem = Pedido.idItem
 GROUP BY
     ItemDoMenu.idItem, ItemDoMenu.Nome;
-    
+
+/* exemplo de uso - monitorar os pedidos que mais saem e os que menos saem para possíveis melhorias */
 SELECT * FROM PedidosPorItemDoMenu;
